30a5479c14ecea8a8554d03b2fc15ad6885ff8a1
teetangh <teetangh@gmail.com>
2024-08-12 14:54:22 +0530

    fixed bucket name

--- cohere/search.py
+++ cohere/search.py
@@ -164,10 +164,10 @@ def main():
         # Get environment variables
         COHERE_API_KEY = get_env_variable('COHERE_API_KEY')
         OPENAI_API_KEY = get_env_variable("OPENAI_API_KEY")
+        CB_HOST = get_env_variable('CB_HOST', 'couchbase://localhost')
         CB_USERNAME = get_env_variable('CB_USERNAME', 'Administrator')
         CB_PASSWORD = get_env_variable('CB_PASSWORD', 'password')
-        CB_BUCKET_NAME = get_env_variable('CB_BUCKET_NAME', 'travel-sample')
-        CB_HOST = get_env_variable('CB_HOST', 'couchbase://localhost')
+        CB_BUCKET_NAME = get_env_variable('CB_BUCKET_NAME', 'vector-search-testing')
         INDEX_NAME = get_env_variable('INDEX_NAME', 'vector_search_cohere')
         
         SCOPE_NAME = get_env_variable('SCOPE_NAME', 'shared')
